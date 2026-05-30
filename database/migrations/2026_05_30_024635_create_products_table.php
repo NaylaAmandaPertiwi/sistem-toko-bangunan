4645e524108681f9333e5d26a370234d3972dd73
NaylaAmandaPertiwi <naylaamandapertiwi@gmail.com>
2026-05-30 10:05:27 +0700

    feat: create products migration with category relationship

--- database/migrations/2026_05_30_024635_create_products_table.php
+++ database/migrations/2026_05_30_024635_create_products_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
 
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onDelete('cascade');
+
         $table->foreignId('category_id')
                   ->constrained('categories')
                   ->onDelete('cascade');
