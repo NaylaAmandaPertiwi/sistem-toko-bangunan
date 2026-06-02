14ae0d113f9ff1082e2ebd777327573f50eecba6
NaylaAmandaPertiwi <naylaamandapertiwi@gmail.com>
2026-06-02 12:05:23 +0700

    feat: create stock incoming migration

---
+++ database/migrations/2026_06_02_045732_create_stock_ins_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_ins', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('nomor_transaksi')->unique();
+
+            $table->foreignId('supplier_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('product_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->date('tanggal_masuk');
+
+            $table->integer('jumlah_masuk');
+
+            $table->decimal('harga_beli',15,2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_ins');
+    }
+};
