b963cbb95da7646295cb5d17b958f7352ce90c39
NaylaAmandaPertiwi <naylaamandapertiwi@gmail.com>
2026-06-04 14:31:50 +0700

    feat: add stock out model

---
+++ app/Models/StockOut.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StockOut extends Model
+{
+    protected $fillable = [
+        'nomor_transaksi',
+        'product_id',
+        'tanggal_keluar',
+        'jumlah_keluar',
+        'harga_jual',
+        'total',
+        'tujuan',
+        'keterangan'
+    ];
+}
